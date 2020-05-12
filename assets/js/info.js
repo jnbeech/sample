b5d9e98cb1440b4d958e899a6e572aadc076b4eb
jnbeech <noreply@github.com>
2020-05-12 14:04:20 -0400

    force apply after obtaining settings.

--- assets/js/info.js
+++ assets/js/info.js
@@ -2,14 +2,16 @@
 /*global $*/
 /*global location*/
 
-(function(){
+(function () {
   var queryDict = {};
-  location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
-  var app = angular.module('benefitCalculator',[]);
+  location.search.substr(1).split("&").forEach(function (item) {
+    queryDict[item.split("=")[0]] = item.split("=")[1]
+  });
+  var app = angular.module('benefitCalculator', []);
 
-  app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+  app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     // Get settings
-    $.getJSON("/sample/assets/settings.json").then(function(settings) {
+    $.getJSON("/sample/assets/settings.json").then(function (settings) {
       $scope.settings = settings;
       $scope.config = settings.config;
       $scope.planYear = settings.config.planYear;
@@ -21,21 +23,22 @@
       $scope.benefits = Object.keys($scope.insurancePlans);
       $scope.planLevels = settings.levels;
       $scope.totalHours = settings.config.salaryHours;
-  });
+      $scope.$apply()
+    });
 
     // Income
     $scope.tcRate = 0;
-    if(queryDict.hasOwnProperty('tcr') && parseFloat(queryDict.tcr) > 0) {
+    if (queryDict.hasOwnProperty('tcr') && parseFloat(queryDict.tcr) > 0) {
       $scope.tcRate = parseFloat(queryDict.tcr);
     }
-      $scope.totalHours = 1840;
+    $scope.totalHours = 1840;
 
     // PTO
     $scope.ptoPlan = 30;
-    if(queryDict.hasOwnProperty('pto') && parseFloat(queryDict.pto) > 0) {
+    if (queryDict.hasOwnProperty('pto') && parseFloat(queryDict.pto) > 0) {
       $scope.ptoPlan = parseFloat(queryDict.pto);
     }
-    $scope.ptoPercent = function() {
+    $scope.ptoPercent = function () {
       return 1 + 1 / $scope.ptoPlan;
     };
 
@@ -44,7 +47,7 @@
     $scope._401kPercent = 6;
     $scope.catch_up = false;
     $scope.retirementMaximums = {};
-    $scope.max401k = function() {
+    $scope.max401k = function () {
       return $scope.catch_up ? ($scope.retirementMaximums.employee + $scope.retirementMaximums.catchup) : $scope.retirementMaximums.employee;
     };
 
@@ -54,11 +57,11 @@
     $scope.plan = {};
     $scope.level = {};
 
-    $scope.stdBenefit = function() {
+    $scope.stdBenefit = function () {
       return Math.min(2500, $scope.benefitsSalary() / 52 * 0.6);
     };
 
-    $scope.ltdBenefit = function() {
+    $scope.ltdBenefit = function () {
       return Math.min(7500, $scope.benefitsSalary() / 12 * 0.6);
     };
 
@@ -73,27 +76,27 @@
     $scope.hsaMaximums = {};
 
     // Functions
-    $scope.payRate = function() {
+    $scope.payRate = function () {
       var rate;
       if ($scope.rbe) {
-        rate = $scope.tcRate / 
-          ($scope._401kEmployerContribution / 100 + 1) / 
-            $scope.ptoPercent();
+        rate = $scope.tcRate /
+          ($scope._401kEmployerContribution / 100 + 1) /
+          $scope.ptoPercent();
       } else {
         rate = $scope.tcRate / $scope.ptoPercent();
       }
       return rate;
     };
 
-    $scope.annualPTOHours = function() {
+    $scope.annualPTOHours = function () {
       return $scope.totalHours / $scope.ptoPlan;
     };
 
-    $scope.annualPTOPay = function() {
+    $scope.annualPTOPay = function () {
       return $scope.annualPTOHours() * $scope.payRate();
     };
 
-    $scope.maxHSA = function() {
+    $scope.maxHSA = function () {
       var base = ($scope.level['Health'] > 0) ? $scope.hsaMaximums.family : $scope.hsaMaximums.individual;
       if ($scope.hsa_catch_up) {
         base += $scope.hsaMaximums.catchup;
@@ -101,7 +104,7 @@
       return base;
     };
 
-    $scope.updateHSA = function(t) {
+    $scope.updateHSA = function (t) {
       switch (t) {
         case "weekly":
           $scope.annualHSA = Math.round(($scope.weeklyHSA * 52) * 100) / 100;
@@ -121,63 +124,63 @@
       }
     };
 
-    $scope.HSAEligible = function() {
-      return ($scope.plan['Health'] != '--' 
-        && $scope.level['Health'] !== undefined
-        && $scope.HSARegExp.test($scope.plan['Health'])
+    $scope.HSAEligible = function () {
+      return ($scope.plan['Health'] != '--' &&
+        $scope.level['Health'] !== undefined &&
+        $scope.HSARegExp.test($scope.plan['Health'])
       );
     };
 
-    $scope.maxOutHSA = function() {
+    $scope.maxOutHSA = function () {
       $scope.annualHSA = $scope.maxHSA();
       $scope.updateHSA('annual');
     };
 
-    $scope.cost = function(year, benefit, plan, level) {
-      if (year === undefined || year == '--' || 
-          benefit === undefined || 
-          plan === undefined || plan == '--' || 
-          level === undefined || level == '--') {
+    $scope.cost = function (year, benefit, plan, level) {
+      if (year === undefined || year == '--' ||
+        benefit === undefined ||
+        plan === undefined || plan == '--' ||
+        level === undefined || level == '--') {
         return 0;
       } else {
         return $scope.insurancePlans[benefit][plan][level];
       }
     };
 
-    $scope.stdCost = function() {
-       if ($scope.plan['stdlife'] !== '--' && $scope.stdRate !== undefined) {
-         return ($scope.stdBenefit() / 10) * $scope.stdRate;
-       } else {
-         return 0;
-       }
+    $scope.stdCost = function () {
+      if ($scope.plan['stdlife'] !== '--' && $scope.stdRate !== undefined) {
+        return ($scope.stdBenefit() / 10) * $scope.stdRate;
+      } else {
+        return 0;
+      }
     };
 
-    $scope.monthyInsuranceRate = function() {
-      var sum = $scope.benefits.reduce(function(c, b) {
-        return c +$scope.cost($scope.planYear, b, $scope.plan[b], $scope.level[b]);
+    $scope.monthyInsuranceRate = function () {
+      var sum = $scope.benefits.reduce(function (c, b) {
+        return c + $scope.cost($scope.planYear, b, $scope.plan[b], $scope.level[b]);
       }, 0);
       return sum + $scope.stdCost();
     };
 
-    $scope.annualInsuranceRate = function() {
+    $scope.annualInsuranceRate = function () {
       return $scope.monthyInsuranceRate() * 12;
     };
 
-    $scope.timeOff = function() {
+    $scope.timeOff = function () {
       var days = ($scope.config.stdHours - $scope.totalHours) / 8;
       return days;
     };
 
-    $scope.grossIncome = function() {
+    $scope.grossIncome = function () {
       return $scope.payRate() * $scope.totalHours + $scope.annualPTOPay();
     };
 
-    $scope.benefitsSalary = function() {
+    $scope.benefitsSalary = function () {
 
       return $scope.tcRate * $scope.config.salaryHours;
     };
 
-    $scope.employee401kContribution = function() {
+    $scope.employee401kContribution = function () {
       if ($scope.rbe) {
         return Math.min($scope.max401k(), $scope.grossIncome() * ($scope._401kPercent / 100));
       } else {
@@ -185,7 +188,7 @@
       }
     };
 
-    $scope.employer401kContribution = function() {
+    $scope.employer401kContribution = function () {
       if ($scope.rbe) {
         return Math.min($scope.grossIncome() * ($scope._401kEmployerContribution / 100), $scope.maxEmployer401kContribution());
       } else {
@@ -193,37 +196,37 @@
       }
     };
 
-    $scope.maxEmployer401kContribution = function() {
+    $scope.maxEmployer401kContribution = function () {
       return ($scope._401kEmployerContribution / 100) * $scope.retirementMaximums.max_comp;
     };
 
-    $scope.total401kContribution = function() {
+    $scope.total401kContribution = function () {
       return $scope.employee401kContribution() + $scope.employer401kContribution();
     };
 
-    $scope.totalHSAContribution = function() {
+    $scope.totalHSAContribution = function () {
       if ($scope.annualHSA > $scope.maxHSA()) {
         $scope.maxOutHSA();
       }
       return $scope.HSAEligible() ? parseFloat($scope.annualHSA) : 0;
     };
 
-    $scope.adjustedGrossIncome = function() {
-      return $scope.grossIncome()
-           - $scope.employee401kContribution()
-           - $scope.annualInsuranceRate()
-           - $scope.totalHSAContribution();
+    $scope.adjustedGrossIncome = function () {
+      return $scope.grossIncome() -
+        $scope.employee401kContribution() -
+        $scope.annualInsuranceRate() -
+        $scope.totalHSAContribution();
     };
 
-    $scope.totalCompensation = function() {
-      return $scope.adjustedGrossIncome()
-           + $scope.total401kContribution()
-           + $scope.annualInsuranceRate()
-           + $scope.totalHSAContribution();
+    $scope.totalCompensation = function () {
+      return $scope.adjustedGrossIncome() +
+        $scope.total401kContribution() +
+        $scope.annualInsuranceRate() +
+        $scope.totalHSAContribution();
     };
   }]);
 })();
 
-$(function() {
+$(function () {
   $(document).tooltip();
 });
